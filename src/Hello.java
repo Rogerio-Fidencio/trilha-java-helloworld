a3f732ec4d283d452ede9cc90479b3dc77e90a9f
Rogério Fidnecio <rogerio.fidencio.dev@gmail.com>
2022-02-08 16:30:18 -0300

    create repository

---
+++ src/Hello.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Hello {   
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Hello world!!");
+		
+		
+		
+		input.close();
+		
+	}
+}
